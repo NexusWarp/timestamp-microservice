e6a026ed110e12cd3bb12154fa88a152cabc3c00
NexusWarp <ik571572@gmail.com>
2024-04-13 02:04:46 +0500

    changes

--- index.js
+++ index.js
@@ -2,52 +2,56 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
-
-
-
-// your first API endpoint... 
+app.get("/api",(req,res)=>{
+  let curDate = new Date();
+    let curMilisec = curDate.getTime();
+    res.json({ unix: curMilisec, utc: `${curDate.toUTCString()}` });
+})
+// your first API endpoint...
 app.get("/api/:date", function (req, res) {
-  if(req.params.date.includes("-")){
-    let param = req.params.date;
-    let cDate = new Date(param);
-    let milisec = cDate.getTime();
-    
-    
-    res.json({"unix":milisec, "utc":`${cDate.toUTCString()}`});
-    
-  }else{
-    
-    let param = parseInt(req.params.date);
-    let cDate = new Date(param);
-
-    res.json({"unix":param, "utc":`${cDate.toUTCString()}`});
-    
-  }
-  
-  let unixCode = 1451001600000;
-  
  
+  if (req.params.date.includes("-")) {
+    let param = req.params.date;
+    let fixDate = new Date(param);
+    if (fixDate == "Invalid Date") {
+      res.json({ error: "Invalid Date" });
+    } else {
+      let milisec = fixDate.getTime();
+      res.json({ unix: milisec, utc: `${fixDate.toUTCString()}` });
+    }
+  } else {
+    console.log(req.params.date.length);
+    let param = parseInt(req.params.date);
+    let fixDate = new Date(param);
+    if(fixDate == "Invalid Date"){
+      res.json({ error: "Invalid Date" });
+      
+    }else{
+      
+      res.json({ unix: param, utc: `${fixDate.toUTCString()}` });
+    }
 
+  }
+
+  
 });
-
-
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
